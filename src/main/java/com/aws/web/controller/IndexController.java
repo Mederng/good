96803048bb6dfca7f898fe279d63069dca731224
U_Young_U <83068670+Mederng@users.noreply.github.com>
2022-12-10 20:54:38 +0900

    Merge remote-tracking branch 'origin/main'

--- src/main/java/com/aws/web/controller/IndexController.java
+++ src/main/java/com/aws/web/controller/IndexController.java
@@ -1,5 +1,6 @@
 package com.aws.web.controller;
 
+import com.aws.config.auth.dto.SessionUser;
 import com.aws.service.posts.PostsService;
 import com.aws.web.dto.PostsResponseDTO;
 import lombok.RequiredArgsConstructor;
@@ -8,17 +9,27 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController
 {
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
 
     @GetMapping("/")
     public String index(Model model)
     {
         model.addAttribute("posts", postsService.findAllDesc());
+
+        SessionUser user = (SessionUser)httpSession.getAttribute("user");
+
+        if(user != null)
+        {
+            model.addAttribute("userName", user.getName());
+        }
 
         return "index";
     }
