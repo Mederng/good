0eaaa469619790fc7cf3f2d6f6107a0f13c7106d
U_Young_U <83068670+Mederng@users.noreply.github.com>
2022-12-12 05:03:07 +0900

    Merge remote-tracking branch 'origin/main'

--- src/main/java/com/aws/web/controller/IndexController.java
+++ src/main/java/com/aws/web/controller/IndexController.java
@@ -1,5 +1,6 @@
 package com.aws.web.controller;
 
+import com.aws.config.auth.LoginUser;
 import com.aws.config.auth.dto.SessionUser;
 import com.aws.service.posts.PostsService;
 import com.aws.web.dto.PostsResponseDTO;
@@ -9,22 +10,17 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import javax.servlet.http.HttpSession;
-
 @RequiredArgsConstructor
 @Controller
 public class IndexController
 {
     private final PostsService postsService;
-    private final HttpSession httpSession;
 
 
     @GetMapping("/")
-    public String index(Model model)
+    public String index(Model model, @LoginUser SessionUser user)
     {
         model.addAttribute("posts", postsService.findAllDesc());
-
-        SessionUser user = (SessionUser)httpSession.getAttribute("user");
 
         if(user != null)
         {
--- src/test/java/com/aws/web/controller/PostsApiControllerTests.java
+++ src/test/java/com/aws/web/controller/PostsApiControllerTests.java
@@ -4,22 +4,29 @@ import com.aws.web.domain.posts.Posts;
 import com.aws.web.domain.posts.PostsRepository;
 import com.aws.web.dto.PostsSaveRequestDTO;
 import com.aws.web.dto.PostsUpdateRequestDTO;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -34,6 +41,22 @@ public class PostsApiControllerTests
     @Autowired
     private PostsRepository postsRepository;
 
+    @Autowired
+    private WebApplicationContext context;
+
+    private MockMvc mvc;
+
+
+    @Before
+    public void setup()
+    {
+        mvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .apply(springSecurity())
+                .build();
+    }
+
+
     @After
     public void tearDown() throws Exception
     {
@@ -41,6 +64,7 @@ public class PostsApiControllerTests
     }
 
 
+    @WithMockUser(roles="USER")
     @Test
     public void Posts_등록된다() throws Exception
     {
@@ -55,10 +79,10 @@ public class PostsApiControllerTests
 
         String url = "http://localhost:" + port + "/api/v1/posts";
 
-        ResponseEntity<Long> responseEntity = restTemplate.postForEntity(url, requestDTO, Long.class);
-
-        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
-        assertThat(responseEntity.getBody()).isPositive();
+        mvc.perform(post(url)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsString(requestDTO)))
+                    .andExpect(status().isOk());
 
         List<Posts> all = postsRepository.findAll();
 
@@ -67,6 +91,7 @@ public class PostsApiControllerTests
     }
 
 
+    @WithMockUser(roles="USER")
     @Test
     public void Posts_수정된다() throws Exception
     {
@@ -88,13 +113,12 @@ public class PostsApiControllerTests
 
         String url = "http://localhost:" + port + "/api/v1/posts/" + updateId;
 
-        HttpEntity<PostsUpdateRequestDTO> requestEntity = new HttpEntity<>(requestDTO);
 
-        ResponseEntity<Long> responseEntity =
-                restTemplate.exchange(url, HttpMethod.PUT, requestEntity, Long.class);
+        mvc.perform(put(url)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsString(requestDTO)))
+                .andExpect(status().isOk());
 
-        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
-        assertThat(responseEntity.getBody()).isPositive();
 
         List<Posts> all = postsRepository.findAll();
 
